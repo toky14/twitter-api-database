3fdecbc6d781ce6bc6f50280860fc3928a769cd7
RABARINIRINA Tokiniaina Michael <rabtoky@gmail.com>
2022-01-20 16:05:11 +0300

    Added database

--- app/apis/tweets.py
+++ app/apis/tweets.py
@@ -1,6 +1,6 @@
 from flask_restx import Namespace, Resource, fields
-from app.db import tweet_repository
 from app.models import Tweet
+from app import db
 
 api = Namespace('tweets')  # Base route
 
@@ -20,7 +20,7 @@ json_new_tweet = api.model('New tweet', {
 class TweetResource(Resource):
     @api.marshal_with(json_tweet)  # Used to control JSON response format
     def get(self, tweet_id):  # GET method
-        tweet = tweet_repository.get(tweet_id)
+        tweet = db.session.query(Tweet).get(tweet_id)
         if tweet is None:
             api.abort(404)  # abort will throw an exception and break execution flow (equivalent to 'return' keyword for an error)
         return tweet, 200
@@ -28,7 +28,7 @@ class TweetResource(Resource):
     @api.marshal_with(json_tweet, code=200)
     @api.expect(json_new_tweet, validate=True)  # Used to control JSON body format (and validate)
     def patch(self, tweet_id):  # PATCH method
-        tweet = tweet_repository.get(tweet_id)
+        tweet = db.session.query(Tweet).get(tweet_id)
         if tweet is None:
             api.abort(404)
 
@@ -39,10 +39,10 @@ class TweetResource(Resource):
         return None, 204
 
     def delete(self, tweet_id):  # DELETE method
-        tweet = tweet_repository.get(tweet_id)
+        tweet = db.session.query(Tweet).get(tweet_id)
         if tweet is None:
             api.abort(404)
-        tweet_repository.remove(tweet_id)
+        db.session.remove(tweet_id)
         return None, 204
 
 @api.route('')  # empty route extension (ie: /tweets)
@@ -55,11 +55,11 @@ class TweetsResource(Resource):
         # body has already been validated using json_new_tweet schema
         text = api.payload['text']
         tweet = Tweet(text)
-        tweet_repository.add(tweet)
+        db.session.add(tweet)
         return tweet, 201
 
     # Here we use marshal_list_with (instead of marshal_with) to return a list of tweets
     @api.marshal_list_with(json_tweet)
     def get(self):  # GET method
-        tweets = tweet_repository.get_all()
+        tweets = db.session.query(Tweet).get_all()
         return tweets, 200
